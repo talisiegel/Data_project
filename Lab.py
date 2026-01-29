d54e8efb1114eda1653d5481da5bbb82744a53fe
talisiegel <tali.siegel.2029@heschel.org>
2026-01-29 13:33:44 -0500

    Add files via upload

---
+++ Lab.py
@@ -0,0 +1,14 @@
+
+import math 
+
+def hypotenuse(a,b):
+    return math.sqrt(a**2 + b**2)
+    
+    
+
+def num_digits(n):
+    return len(str(abs(n)))
+
+def tip_amount(total,percent):
+    return total * (percent / 100)
+
